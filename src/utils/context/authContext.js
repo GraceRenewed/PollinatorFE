366497ef219f66355e2a7ebd41586e63d961619c
Christina Vieau <cvieau@live.com>
2025-07-01 20:26:29 -0400

    added file corrections

--- src/utils/context/authContext.js
+++ src/utils/context/authContext.js
@@ -4,8 +4,18 @@
 
 import React, { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import { firebase } from '@/utils/client';
+import { checkUser } from '@/utils/auth';
 
 const AuthContext = createContext();
+const [oAuthUser, setOAuthUser] = useState(null);
+
+const updateUser = useMemo(
+  () => (uid) =>
+    checkUser(uid).then((gamerInfo) => {
+      setUser({ fbUser: oAuthUser, ...gamerInfo });
+    }),
+  [oAuthUser],
+);
 
 AuthContext.displayName = 'AuthContext'; // Context object accepts a displayName string property. React DevTools uses this string to determine what to display for the context. https://reactjs.org/docs/context.html#contextdisplayname
 
@@ -20,23 +30,25 @@ function AuthProvider(props) {
   useEffect(() => {
     firebase.auth().onAuthStateChanged((fbUser) => {
       if (fbUser) {
-        setUser(fbUser);
+        setOAuthUser(fbUser);
+        checkUser(fbUser.uid).then((gamerInfo) => {
+          setUser({ fbUser, uid: fbUser.uid, ...gamerInfo });
+        });
       } else {
+        setOAuthUser(false);
         setUser(false);
       }
     }); // creates a single global listener for auth state changed
   }, []);
 
-  const value = useMemo(
-    // https://reactjs.org/docs/hooks-reference.html#usememo
-    () => ({
-      user,
-      userLoading: user === null,
-      // as long as user === null, will be true
-      // As soon as the user value !== null, value will be false
-    }),
-    [user],
-  );
+    const value = useMemo(
+      () => ({
+        user,
+        userLoading: user === null || oAuthUser === null,
+        updateUser,
+      }),
+      [user, oAuthUser, updateUser],
+    );
 
   return <AuthContext.Provider value={value} {...props} />;
 }
